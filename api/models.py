118d6f6fb7a428365c7d248cd89dd8f9a345fbb1
Taha Goda <taha.mostafa.goda@gmail.com>
2023-01-18 20:36:44 +0200

    [FIX]:models ondelete type

--- api/models.py
+++ api/models.py
@@ -13,5 +13,5 @@ class Record(models.Model):
     signature_date = models.DateField()
     title = models.CharField(max_length=140)
     sectors = models.ManyToManyField(to=Sector)
-    country = models.ForeignKey(to=Country)
+    country = models.ForeignKey(to=Country,on_delete=models.CASCADE)
     signed_amount = models.CharField()

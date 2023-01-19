269b42fbe93cb60ad8d174e1627a2641c0bdadd4
Taha Goda <taha.mostafa.goda@gmail.com>
2023-01-19 19:20:59 +0200

    [ADD]:command save records after scraping

--- scrap/management/commands/do_scrap.py
+++ scrap/management/commands/do_scrap.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from scrap.services.scrap_records import ScrapRecords
+from scrap.services.save_records import RecordsRepo
 import logging
 
 logger = logging.getLogger(__name__)
@@ -7,8 +8,9 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print("== Start handle command ==")
+        logger.warning("== Start handle command ==")
         records = ScrapRecords().get_records()
-        print("== Records ==")
+        for record in records:
+            RecordsRepo().save_record(record)
         print(records)
         # return records

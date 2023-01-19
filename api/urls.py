bf0161f317b718712d6657f9ffa7f03123c4d1a3
Taha Goda <taha.mostafa.goda@gmail.com>
2023-01-19 19:20:18 +0200

    [FIX]:api/urls.py apply valid pattern

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import *
 
-
+app_name = 'api'
 urlpatterns = [
-    path("records", Records.as_view(), name="record"),
-    path("country", Countries.as_view(), name="country"),
-    path("sector", Sectors.as_view(), name="sector"),
+    path("records/", Records.as_view(), name="record"),
+    path("country/", Countries.as_view(), name="country"),
+    path("sector/", Sectors.as_view(), name="sector"),
 ]

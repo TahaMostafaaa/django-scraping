a6a384f3a591ebe908f6808119f4d4947e8aad4f
Taha Goda <taha.mostafa.goda@gmail.com>
2023-01-20 09:39:51 +0200

    [REOMVE]:report.py un used comments and libraries

--- reporting/services/report.py
+++ reporting/services/report.py
@@ -2,9 +2,6 @@
 import xlsxwriter
 import logging
 from scrap.models import Record
-from django.core.serializers import serialize
-from collections import defaultdict
-import locale
 
 logger = logging.getLogger(__name__)
 
@@ -24,9 +21,6 @@ class Report:
         for idx,header in enumerate(headrers_list):
             worksheet.write(0,idx,str(header).capitalize())
         
-        # countries = defaultdict(float)
-        # countries = set()
-        # adding data
 
         for idx1,entry in enumerate(data):
             for idx2, header in enumerate(headrers_list):
@@ -42,30 +36,12 @@ class Report:
         chart1 = workbook.add_chart({'type': 'column'})
 
         # Configure the first series.
-        # chartsheet.write_column('A1', list(countries))
-        # chartsheet.write_column('Sectors', data[1])
-        # chartsheet.write_column('Amount', data[2])
+
         chart1.add_series({'values': f'={worksheet_name}!$E$2:$E$25',
                             'categories': f'={worksheet_name}!$D$2:$D$25',
                            'name': 'Country & Amount'
                            })
-        # chart1.add_series({'values': f'={worksheet_name}!$E$1:$E$25',
-        #                    'name': 'Amount'
-        #                    })
-        # chart1.add_series({'values': f'={worksheet_name}!$B$1:$B$5'})
-        # chart1.add_series({'values': f'={worksheet_name}!$C$1:$C$5'})
 
-        # Configure a second series. Note use of alternative syntax to define ranges.
-        # chart1.add_series({
-        #     'name':       [worksheet_name, 0, 2],
-        #     'categories': [worksheet_name, 1, 0, 6, 0],
-        #     'values':     [worksheet_name, 1, 2, 6, 2],
-        # })
-
-        # # Add a chart title and some axis labels.
-        # chart1.set_title({'name': 'Results of Countries loans'})
-        # chart1.set_x_axis({'name': 'Loans number'})
-        # chart1.set_y_axis({'name': 'Countries'})
 
         # Set an Excel chart style.
         chart1.set_style(11)
